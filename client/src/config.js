9a3b6d478df7b82e2e880d263d9aa8de1adf0e67
ssrawat1 <ssr911999@gmail.com>
2025-08-21 22:45:53 +0530

    fix: properly format CloudFront private key in .env with escaped newlines

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,2 @@
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-console.log({BACKEND_URL})
-export const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+ export const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
